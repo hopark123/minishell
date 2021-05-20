80183dceb58b95e7e05b9642634cee58a8bbdb23
hjpark <hjpark@hjparkui-MacBookAir.local>
2021-05-20 21:15:46 +0900

    split end

--- src/passing/ft_listjoin.c
+++ src/passing/ft_listjoin.c
@@ -6,38 +6,45 @@
 /*   By: hopark <hopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 19:38:13 by hopark            #+#    #+#             */
-/*   Updated: 2021/05/20 20:03:40 by hopark           ###   ########.fr       */
+/*   Updated: 2021/05/20 21:09:48 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
+t_list	*ft_list_join(t_list *first, t_list *second)
+{
+	char	*new_str;
+	t_list	*new_list;
+
+	if (!ft_malloc(&new_list, sizeof(t_list)))
+		return (NULLPTR);
+	new_str = ft_strjoin(first->str, second->str);
+	ft_free(first->str);
+	ft_free(second->str);
+	new_list->str = new_str;
+	new_list->next = second->next;
+	new_list->prev = first->prev;
+	if (first->prev)
+		first->prev->next = new_list;
+	if (second->next)
+		second->next->prev = new_list;
+	ft_free(first);
+	ft_free(second);
+	return (new_list);
+}
+
 int	ft_listjoin(t_built *built)
 {
 	t_list	*temp_l;
-	t_list	*temp_l2;
-	char	*temp_s;
 
 	temp_l = built->command;
 	while (temp_l)
 	{
-		//write(1,"A\n",2);
-		if (temp_l->str && *(temp_l->str) != ' ' && temp_l->next && *(temp_l->next->str) != ' ')
-		{
-			//write(1,"B\n",2);
-
-			temp_s = ft_strjoin(temp_l->str, temp_l->next->str);
-			ft_free(temp_l->str);
-			temp_l->str = temp_s;
-			ft_putstr_fd(temp_l->str,1 , 0);
-			write(1,"\n",1);
-			ft_listdelone(&(temp_l->next));
-			write(1,"C\n",2);
-		}
-		temp_l = temp_l->next;
-		write(1,"D\n",2);
-		printf("@@%s@@\n", temp_l->str);
+		if (*(temp_l->str) != ' ' && temp_l->next && *(temp_l->next->str) != ' ')
+			temp_l = ft_list_join(temp_l, temp_l->next);
+		else
+			temp_l = temp_l->next;
 	}
-		write(1,"E\n",2);
 
 }
