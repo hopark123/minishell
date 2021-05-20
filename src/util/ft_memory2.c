f61684e12e6e03c98e19f8f18483c69a65f453ed
hjpark <hjpark@hjparkui-MacBookAir.local>
2021-05-20 14:12:17 +0900

    strn

--- src/util/ft_memory2.c
+++ src/util/ft_memory2.c
@@ -6,30 +6,11 @@
 /*   By: hopark <hopark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 15:34:52 by hopark            #+#    #+#             */
-/*   Updated: 2021/05/20 13:27:18 by hopark           ###   ########.fr       */
+/*   Updated: 2021/05/20 14:12:05 by hopark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
-
-int	ft_strncmp2(const char *s1, const char *s2, int n)
-{
-	int				i;
-	unsigned char	*c1;
-	unsigned char	*c2;
-
-	c1 = (unsigned char *)s1;
-	c2 = (unsigned char *)s2;
-	i = 0;
-	n = ft_strlen(s2);
-	while (i < n)
-	{
-		if (c1[i] != c2[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
 int	ft_strncmp(const char *s1, const char *s2, int n)
 {
@@ -51,6 +32,52 @@ int	ft_strncmp(const char *s1, const char *s2, int n)
 		i++;
 	}
 	return (1);
+}
+
+int	ft_strncmp2(const char *s1, const char *s2, int n)
+{
+	int				i;
+	unsigned char	*c1;
+	unsigned char	*c2;
+
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
+	i = 0;
+	n = ft_strlen(s2);
+	while (i < n)
+	{
+		if (c1[i] != c2[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_strncmp3(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*f;
+	unsigned char	*b;
+
+	i = 0;
+	f = (unsigned char *)s1;
+	b = (unsigned char *)s2;
+	while (i < n && f[i] != '\0' && b[i] != '\0')
+	{
+		if (f[i] > b[i])
+			return (1);
+		if (b[i] > f[i])
+			return (-1);
+		i++;
+	}
+	if (i < n)
+	{
+		if (f[i] != '\0')
+			return (1);
+		if (b[i] != '\0')
+			return (-1);
+	}
+	return (0);
 }
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
