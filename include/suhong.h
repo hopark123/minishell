6c363f4b9a20724cfeb28e2d00dc2a7f27b07f22
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2021-06-24 16:51:41 +0900

    pipe

--- include/suhong.h
+++ include/suhong.h
@@ -7,7 +7,7 @@
 int	ft_execve(t_built *built, t_list *env_list);
 int	check_file_exist(char *name);
 void	ft_del_blank3(t_built *built);
-int	ft_subshell(t_built *built, t_list *env_list, int *p_fd);
+int	ft_subshell(t_built *built, t_list *env_list, int *fd);
 int ft_start(t_built *built, t_list *env_list);
 
 #endif
--- src/engine/ft_subshell.c
+++ src/engine/ft_subshell.c
@@ -1,49 +1,68 @@
 
 #include "head.h"
 
+static void	close_pip(int pip_in, int pip_out)
+{
+	if (pip_in >= 0)
+		close(pip_in);
+	if (pip_out >= 0)
+		close(pip_out);
+}
+
 int ft_start(t_built *built, t_list *env_list)
 {
-	int		p_fd[2];
+	int	fd[2];
 
-	p_fd[0] = STDIN;
-	p_fd[1] = STDOUT;
 	if (built->next && built->next->command->str[0] == '|')
 	{
+		if (pipe(fd) < 0)
+		{
+			perror("pipeerror");
+			return (ERROR);
+		}
 		while (built)
 		{
-			// write(1,"3@",2);
-			ft_subshell(built, env_list, p_fd);
-			p_fd[1] = dup(p_fd[0]);
-			// printf("%d\n", p_fd[1]);
-			// get_next_line(p_fd[0], &line);
-			// printf("%s\n", line);
+			ft_subshell(built, env_list, fd);
 			if (built->next && built->next->command->str[0] != '|')
 				break ;
+			// printf("[%s]end subshell\n", built->command->str);
+			dup2 (STDIN , fd[1]);
+			// printf("[%s]end dup2\n", built->command->str);
 			built = built->next;
 		}
-		ft_close(p_fd[1]);
+		close_pip(fd[0], fd[1]);
 	}
 	else
-	{
-		write(1,"&&",2);
 		ft_execute(built, env_list);
-	}
 	return (1);
 }
 
-int	ft_subshell(t_built *built, t_list *env_list, int *p_fd)
+static void	do_piping(int pip_in, int pip_out)
+{
+	// printf("pipin:%d pipout:%d \n", pip_in, pip_out);
+	if (pip_in != -1)
+	{
+		if (dup2(pip_in, STDIN) < 0)
+			perror("pipe_in:error");
+		ft_close(pip_in);
+
+	}
+	if (pip_out != -1)
+	{
+		if (dup2(pip_out, STDOUT) < 0)
+			perror("pipe_out:error");
+		ft_close(pip_out);
+
+	}
+}
+
+int	ft_subshell(t_built *built, t_list *env_list, int *fd)
 {
 	int	status;
 	int	w_status;
 	pid_t	pid;
 	pid_t	w_pid;
-	int	fd[2];
-
-	if (pipe(fd) < 0)
-	{
-		perror("pipeerror");
-		return (ERROR);
-	}
+	
 	pid = fork();
 	if (pid < 0)
 	{
@@ -52,39 +71,17 @@ int	ft_subshell(t_built *built, t_list *env_list, int *p_fd)
 	}
 	if (pid == 0) //자식
 	{
-		dup2(fd[0], STDIN);
-		dup2(fd[1], STDOUT);
-		close(fd[0]);
+		do_piping(fd[0], fd[1]);
+		// write(1, "aaa\n", 4);
 		status = ft_execute(built, env_list);
-		// if (!built->next || built->next->command->str[0] != '|')
-		// {
-		// 	exit(status);
-		// }
-		// exit(status | ft_subshell(built->next, env_list));
+		// write(1, "bbb\n", 4);
 		exit(status);
 	}
 	else // 부모
 	{
-		// dup2(fd[1], STDOUT);
-		dup2(p_fd[1], fd[0]);
-		// ft_close(fd[0]);
 		w_pid = waitpid(pid, &w_status, WUNTRACED);
 		while (!WIFEXITED(w_status) && !WIFSIGNALED(status))
 			w_pid = waitpid(pid, &w_status, WUNTRACED);
-		
-		char *line;
-		write(1,"a",1);
-		printf("d : %d %d\n", p_fd[0], p_fd[1]);
-		get_next_line(p_fd[1], &line);
-		write(1,"c",1);
-		printf("[%s]\n", line);
-		
-		p_fd[0] = fd[1]; 
-		// p_fd[0] = dup(fd[1]);
-		
-		
-		ft_close(p_fd[1]);
-		// dup2(fd[1], p_fd[0]);
 	}
 	return (w_status);
 }
