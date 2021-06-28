d0c3de027a9acc89199464fc0d1bf007a80c96e9
hjpark <hjpark@hjparkui-MacBookAir.local>
2021-06-28 20:08:07 +0900

    1

--- include/suhong.h
+++ include/suhong.h
@@ -7,7 +7,6 @@
 int	ft_execve(t_built *built, t_list *env_list);
 int	check_file_exist(char *name);
 void	ft_del_blank3(t_built *built);
-int	ft_subshell(t_built *built, t_list *env_list, int *fd, int *mini);
 int ft_start(t_built *built, t_list *env_list);
 
 #endif
