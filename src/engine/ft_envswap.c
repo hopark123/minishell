9f019be30995b09b7594d7fa0f3d251f33cc00dd
hjpark <hjpark@hjparkui-MacBookAir.local>
2021-07-02 18:30:48 +0900

    envswap

--- src/engine/ft_envswap.c
+++ src/engine/ft_envswap.c
@@ -6,7 +6,7 @@
 /*   By: hjpark <hjpark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 15:35:32 by hopark            #+#    #+#             */
-/*   Updated: 2021/07/02 17:39:32 by hjpark           ###   ########.fr       */
+/*   Updated: 2021/07/02 18:29:43 by hjpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,10 @@ static int	ft_envlen(char *str, t_list *env_list)
 	i = 0;
 	while (str[i])
 	{
-		if (ft_strchr("$ ><|;\'\"=", str[i]))
-			return (i);
 		if (ft_getenv(env_list, str, i + 1))
 			return (i + 1);
+		if (ft_strchr("$ ><|;\'\"=", str[i]))
+			return (i);
 		i++;
 	}
 	return (i);
@@ -52,10 +52,11 @@ static char	*ft_strswap(t_list **list, char *old, char *new, int oldlen)
 {
 	char	*res;
 
-	if (!ft_malloc(&res, sizeof(ft_strlen((*list)->str) \
-									- oldlen + ft_strlen(new) + 1)))
+	if (!ft_malloc(&res, sizeof(char) * (ft_strlen((*list)->str) \
+									- oldlen + ft_strlen(new))))
 		return (0);
-	// fprintf(stderr, "[%d][%d][%s]\n", ft_strlen((*list)->str), oldlen, old);
+	// fprintf(stderr, "bf[%s]|old[%s][%d]|new[%s]|af[%s]\n", (*list)->str, old, oldlen, new, res);
+	// fprintf(stderr, "[%d][%d][%s][%d][%d]\n", ft_strlen((*list)->str), oldlen, old, ft_strlen((*list)->str) - oldlen + ft_strlen(new), ft_strlen(new));
 	ft_memcpy(res, (*list)->str, old - (*list)->str);
 	// fprintf(stderr, "res1[%s][%ld]\n", res, old - (*list)->str);
 	ft_memcpy(res + (old - (*list)->str), new, ft_strlen(new));
@@ -65,17 +66,20 @@ static char	*ft_strswap(t_list **list, char *old, char *new, int oldlen)
 												ft_strlen(old + oldlen + 1));
 	// fprintf(stderr, "res3[%s][%s][%d]\n", res, old + oldlen + 1,ft_strlen(old + oldlen + 1));
 
-	res[ft_strlen((*list)->str) - oldlen - 1 + ft_strlen(new)] = 0;
+	res[ft_strlen((*list)->str) - oldlen + ft_strlen(new) - 1] = 0;
 	// fprintf(stderr, "res4[%s][%d]\n", res, ft_strlen((*list)->str) - oldlen - 1 + ft_strlen(new));
-
-	// fprintf(stderr, "bf[%s]|old[%s][%d]|new[%s]|af[%s]\n", (*list)->str, old, oldlen, new, res);
+	write(2,"{{",2);
+	ft_putstr_fd((*list)->str, 2, 0);
+	write(2,"}}",2);
+	fprintf(stderr, "bf[%p]/|old[%s][%d]|new[%s]|af[%s]\n", (*list)->str, old, oldlen, new, res);
 	ft_free((*list)->str);
 	ft_free(new);
 	(*list)->str = res;
-	write(1,"\n",1);
 	return (res);
 }
-
+// echo "$USER2$TE$AE=lol$AT"
+// echo "$USER2"
+// echo "$TE$"
 int	*ft_envswap(t_built *built, t_list *env_list)
 {
 	t_list	*temp_l;
@@ -90,15 +94,18 @@ int	*ft_envswap(t_built *built, t_list *env_list)
 			old = ft_strchr(temp_l->str, '$');
 		if (old)
 		{
+			fprintf(stderr, "@bf[%p]\n",temp_l->str);
 			oldlen = ft_envlen(old + 1, env_list);
+			fprintf(stderr, "@2f[%s]\n",temp_l->str);
 			new = ft_getenv(env_list, old + 1, oldlen);
-			// fprintf(stderr, "[%s]\n",temp_l->str);
+			fprintf(stderr, "@3f[%s]\n",temp_l->str);
 			ft_strswap(&temp_l, old, new, oldlen);
-			old += oldlen + 1;
+			fprintf(stderr, "@4f[%s]\n",temp_l->str);
 		}
 		else
 			temp_l = temp_l->next;
 		old = 0;
+		write(1,"\n",1);
 	}
 	return (0);
 }
